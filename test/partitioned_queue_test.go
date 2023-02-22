c712b430db687f2d64d4b49cffb77577655f9596
Mustafa Al-Sakkaf <malsakkaf@dev2-36.sol-local>
2023-02-22 10:52:15 -0500

    SOl-61096: added extra test to validate partitionGroupId resent by APU in reconnection

--- test/partitioned_queue_test.go
+++ test/partitioned_queue_test.go
@@ -17,33 +17,34 @@
 package test
 
 import (
-	"time"
+    "time"
     "fmt"
     "strconv"
 
-	"solace.dev/go/messaging"
-	"solace.dev/go/messaging/pkg/solace"
-	"solace.dev/go/messaging/pkg/solace/config"
-	"solace.dev/go/messaging/pkg/solace/metrics"
-	"solace.dev/go/messaging/pkg/solace/resource"
-	//"solace.dev/go/messaging/pkg/solace/subcode"
-	"solace.dev/go/messaging/test/helpers"
-	"solace.dev/go/messaging/test/testcontext"
-	"solace.dev/go/messaging/pkg/solace/message"
+    "solace.dev/go/messaging"
+    "solace.dev/go/messaging/pkg/solace"
+    "solace.dev/go/messaging/pkg/solace/config"
+    "solace.dev/go/messaging/pkg/solace/metrics"
+    "solace.dev/go/messaging/pkg/solace/resource"
+    //"solace.dev/go/messaging/pkg/solace/subcode"
+    "solace.dev/go/messaging/test/helpers"
+    "solace.dev/go/messaging/test/testcontext"
+    "solace.dev/go/messaging/pkg/solace/message"
 
-	sempconfig "solace.dev/go/messaging/test/sempclient/config"
+    sempconfig "solace.dev/go/messaging/test/sempclient/config"
 
-	. "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/ginkgo/v2"
     . "github.com/onsi/gomega"
 )
 
 var _ = Describe("Partitioned Queue Tests", func() {
     var queueName string = "partitioned_queue_test"
     var topicName string = "partitioned_queue_topic_test"
-
+    var rebalanceDelay int64 = 5
+    var partitionCount int32 = 3
     Context("queue has three partitions and rebalance delay of 1 second", func() {
         BeforeEach(func() {
-            helpers.CreatePartitionedQueue(queueName, 3, 1, topicName)
+            helpers.CreatePartitionedQueue(queueName, partitionCount, rebalanceDelay, topicName)
         })
     
         AfterEach(func() {
@@ -112,21 +113,21 @@ var _ = Describe("Partitioned Queue Tests", func() {
 
             Eventually(func() uint64 {
                 return receiverOneMetrics.GetValue(metrics.PersistentMessagesReceived)
-                }).Should(BeNumerically(">=", 2))
+                }).WithTimeout(10 * time.Second).Should(BeNumerically(">=", 2))
 
             Eventually(func() uint64 {
                 return receiverTwoMetrics.GetValue(metrics.PersistentMessagesReceived)
-                }).Should(BeNumerically(">=", 2))
+                }).WithTimeout(10 * time.Second).Should(BeNumerically(">=", 2))
 
             Eventually(func() uint64 {
                 return receiverThreeMetrics.GetValue(metrics.PersistentMessagesReceived)
-                }).Should(BeNumerically(">=", 2))
+                }).WithTimeout(10 * time.Second).Should(BeNumerically(">=", 2))
 
             Eventually( func() uint64 {
                 totalMessagesReceived := receiverOneMetrics.
                 GetValue(metrics.PersistentMessagesReceived) + receiverTwoMetrics.GetValue(metrics.PersistentMessagesReceived) + receiverThreeMetrics.GetValue(metrics.PersistentMessagesReceived)
                 return totalMessagesReceived 
-                }).Should(Equal(publisherMetrics.GetValue(metrics.TotalMessagesSent)))
+                }).WithTimeout(10 * time.Second).Should(Equal(publisherMetrics.GetValue(metrics.TotalMessagesSent)))
 
             Expect(receiverOne.Terminate(10 * time.Second)).ToNot(HaveOccurred())
             Expect(receiverTwo.Terminate(10 * time.Second)).ToNot(HaveOccurred())
@@ -186,7 +187,7 @@ var _ = Describe("Partitioned Queue Tests", func() {
             Expect(receiverTwo.Start()).ToNot(HaveOccurred())
             Expect(receiverThree.Start()).ToNot(HaveOccurred())
 
-            time.Sleep(3 * time.Second)
+            time.Sleep(10 * time.Second)
 
             testcontext.SEMP().Config().QueueApi.UpdateMsgVpnQueue(
                 testcontext.SEMP().ConfigCtx(),
@@ -198,11 +199,124 @@ var _ = Describe("Partitioned Queue Tests", func() {
                 nil,
             )
               
-            Eventually(ch).WithTimeout(3 * time.Second).Should(BeClosed())
+            Eventually(ch).WithTimeout(10 * time.Second).Should(BeClosed())
 
             Expect(receiverOne.Terminate(10 * time.Second)).ToNot(HaveOccurred())
             Expect(receiverTwo.Terminate(10 * time.Second)).ToNot(HaveOccurred())
             Expect(receiverThree.Terminate(10 * time.Second)).ToNot(HaveOccurred())
+        })
+        It("rebinds to same partition after reconnect within rebalance delay", func () {
+            var messagingServices[3]solace.MessagingService
+            for i := 0; i < 3; i++{
+                if i == 2 {
+                    messagingServices[i] = helpers.BuildMessagingService(messaging.NewMessagingServiceBuilder().
+                                          FromConfigurationProvider(helpers.ToxicConfiguration()))
+                } else {
+                    messagingServices[i] = helpers.BuildMessagingService(messaging.NewMessagingServiceBuilder().
+                                           FromConfigurationProvider(helpers.DefaultConfiguration()))
+                }
+
+                helpers.ConnectMessagingService(messagingServices[i])
+            }
+
+            defer func() {
+                for i := 0; i < 3; i++{
+                    helpers.DisconnectMessagingService(messagingServices[i])
+                }
+            }()
+
+            var partitionKeys[9]string
+            for i := 0; i < 9; i++{
+                partitionKeys[i] = "key_"+ strconv.Itoa(i)
+            }
+
+            publisher := helpers.NewPersistentPublisher(messagingServices[0])
+            messageBuilder := messagingServices[0].MessageBuilder()
+            publisher.Start()
+            
+            publisherMetrics := messagingServices[0].Metrics()
+            publishMessages := func (firstConnectionAttempt bool) {
+                for i := 0; i < 18; i++{
+                    msg, _ := messageBuilder.WithProperty(config.MessageProperty(config.QueuePartitionKey), partitionKeys[i % 9]).BuildWithStringPayload("Hi Solace")
+                    publisher.Publish(msg, resource.TopicOf(topicName), nil, nil)
+                }
+
+                if firstConnectionAttempt {
+                    Eventually(func() uint64 {
+                        return  publisherMetrics.GetValue(metrics.TotalMessagesSent)
+                        }).WithTimeout(30 * time.Second).Should(BeNumerically("==", 18))
+                } else {
+                    Eventually(func() uint64 {
+                        return  publisherMetrics.GetValue(metrics.TotalMessagesSent)
+                        }).WithTimeout(30 * time.Second).Should(BeNumerically("==", 36))
+                }
+            }
+
+            publishMessages(true)
+            
+            partitionedQueue := resource.QueueDurableNonExclusive(queueName)
+            receiverOne, _ := messagingServices[1].CreatePersistentMessageReceiverBuilder().
+                                                        WithSubscriptions(resource.TopicSubscriptionOf(topicName)).Build(partitionedQueue)
+            receiverOne.Start()
+            receiverOnePartitionKeys := make([]string, 0, 18)
+            receiverOneMessageHandler := func (message message.InboundMessage) {
+                partitionKey, _ := message.GetProperty("JMSXGroupID")
+                partitionKeyValue := fmt.Sprint(partitionKey)
+                receiverOnePartitionKeys = append(receiverOnePartitionKeys, partitionKeyValue)
+            }
+
+            receiverTwo, _ := messagingServices[2].CreatePersistentMessageReceiverBuilder().
+                                                        WithSubscriptions(resource.TopicSubscriptionOf(topicName)).Build(partitionedQueue)
+            receiverTwo.Start()
+            receiverTwoMessageHandler := func (message message.InboundMessage){
+                fmt.Println("Received message in receiverTwo")               
+            }
+
+            receiverOne.ReceiveAsync(receiverOneMessageHandler)
+            receiverTwo.ReceiveAsync(receiverTwoMessageHandler)
+
+            receiverOneMetrics := messagingServices[1].Metrics()
+            receiverTwoMetrics := messagingServices[2].Metrics()
+
+            Eventually( func() uint64 {
+                totalMessagesReceived := receiverOneMetrics.GetValue(metrics.PersistentMessagesReceived) + receiverTwoMetrics.GetValue(metrics.PersistentMessagesReceived)
+                return totalMessagesReceived 
+                }).WithTimeout(30 * time.Second).Should(Equal(publisherMetrics.GetValue(metrics.TotalMessagesSent)))
+
+            partitionKeysBeforeDisconnect := make([]string, len(receiverOnePartitionKeys))
+            copy(receiverOnePartitionKeys, partitionKeysBeforeDisconnect)
+            receiverOnePartitionKeys = receiverOnePartitionKeys[:0]
+            
+            reconnectionListenerChan := make(chan struct{})
+            messagingServices[2].AddReconnectionListener(func(even solace.ServiceEvent) {
+                close(reconnectionListenerChan)
+            })
+
+            reconnectAttemptListenerChan := make(chan struct{})
+            messagingServices[2].AddReconnectionAttemptListener(func(event solace.ServiceEvent) {
+                testcontext.Toxi().SMF().Enable()                
+                close(reconnectAttemptListenerChan)
+				})
+
+            //temporarily disconnect receiverTwo
+            testcontext.Toxi().SMF().Disable()
+            
+            Eventually(reconnectionListenerChan).WithTimeout(30 * time.Second).Should(BeClosed())
+    
+            //republish messages
+            publishMessages(false)
+
+            Eventually( func() uint64 {
+                totalMessagesReceived := receiverOneMetrics.GetValue(metrics.PersistentMessagesReceived) + receiverTwoMetrics.GetValue(metrics.PersistentMessagesReceived)
+                return totalMessagesReceived 
+                }).WithTimeout(30 * time.Second).Should(BeNumerically(">=", publisherMetrics.GetValue(metrics.TotalMessagesSent)))
+
+            partitionKeysAfterReconnection := make([]string, len(receiverOnePartitionKeys))
+            copy(receiverOnePartitionKeys, partitionKeysAfterReconnection)
+
+            Expect(partitionKeysBeforeDisconnect).Should(Equal(partitionKeysAfterReconnection))
+            Expect(receiverOne.Terminate(10 * time.Second)).ToNot(HaveOccurred())
+            Expect(receiverTwo.Terminate(10 * time.Second)).ToNot(HaveOccurred())
         })
     })
 })
