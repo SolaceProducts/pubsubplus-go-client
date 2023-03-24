59d2bb95fd53f3e7d527b5404b3c81240340e6d0
Chris Morgan <Christopher.Morgan@solacesystems.com>
2023-03-24 17:43:30 -0400

    SOL-88879: Added broker diagnostics gathering and tests now output the broker docker logs on testcontainer teardown for tracing
    Signed-off-by: Chris Morgan <Christopher.Morgan@solacesystems.com>

--- test/testcontext/test_context_testcontainers.go
+++ test/testcontext/test_context_testcontainers.go
@@ -132,16 +132,63 @@ func (context *testContainersTestContext) Setup() error {
 func (context *testContainersTestContext) Teardown() error {
 	fmt.Println()
 	fmt.Println("-- TESTCONTAINERS TEARDOWN --")
+	pubsubHostname := context.config.TestContainers.BrokerHostname
 	err := context.toxi.teardown()
 	if err != nil {
 		fmt.Println("Encountered error tearing down toxiproxy: " + err.Error())
 	}
+	rc, output, err := context.dockerLogs(pubsubHostname)
+	if err == nil {
+		fmt.Println("Broker logs for " + pubsubHostname + ":")
+		fmt.Println(output)
+	} else {
+		fmt.Println("Encountered error getting docker logs from " + pubsubHostname + ": rc=" + string(rc) + " err:" + err.Error())
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Encountered error getting working directory for " + pubsubHostname + " diagnostics err:" + err.Error())
+	}
+
+	err = context.gatherBrokerDiagnostics(path.Join(wd, "diagnostics.tgz"))
+	if err != nil {
+		fmt.Println("Encountered error getting " + pubsubHostname + " diagnostics err:" + err.Error())
+	}
+
 	composeResult := context.compose.Down()
 	if composeResult.Error != nil {
 		err = composeResult.Error
 		fmt.Println("Encountered error tearing down docker compose: " + composeResult.Error.Error())
 	}
 	fmt.Println("-- TESTCONTAINERS TEARDOWN COMPLETE --")
+	return err
+}
+
+func (context *testContainersTestContext) gatherBrokerDiagnostics(destinationPath string) error {
+	fmt.Println()
+	pubsubHostname := context.config.TestContainers.BrokerHostname
+	// gather all important infomation and logs from pubsubHostname container
+	fmt.Println("Run gather-diagnostics for " + pubsubHostname + "...")
+	resp, output, err := context.dockerExec(pubsubHostname, []string{"/bin/bash", "-l", "-c", "\"gather-diagnostics\""})
+	if err != nil {
+		return err
+	}
+	fmt.Println(output)
+	if resp != 0 {
+		return fmt.Errorf("failed to gather %s diagnostics", pubsubHostname)
+	}
+	fmt.Println("Gathered gather-diagnostics for " + pubsubHostname)
+	// extract diagnostic to host
+	// first get absolute path from container
+	resp, diagnosticPath, err := context.dockerExec(pubsubHostname, []string{"/bin/bash", "-l", "-c", "ls -rt /usr/sw/jail/logs/gather-diagnostics*.tgz | tail -n 1"})
+	//resp, diagnosticPath, err := context.dockerExec(pubsubHostname, []string{"/bin/bash", "-l", "-c", " realpath $(ls -rt /usr/sw/jail/logs/gather-diagnostics*.tgz | tail -n 1)"})
+	if err != nil {
+		return err
+	}
+	if resp != 0 {
+		return fmt.Errorf("Failed to locate %s diagnostics", pubsubHostname)
+	}
+	fmt.Println("Exacting gather-diagnostics " + diagnosticPath + " for " + pubsubHostname + " to " + destinationPath + "...")
+	err = context.dockerCpToHost(pubsubHostname, strings.TrimSpace(diagnosticPath), destinationPath)
 	return err
 }
 
@@ -285,9 +332,33 @@ func (ctx *testContainersTestContext) dockerCp(sourceContainer, sourcePath, dest
 	return nil
 }
 
+func (ctx *testContainersTestContext) dockerCpToHost(sourceContainer, sourcePath, destPath string) error {
+	hostFileName := destPath
+	fmt.Println("Copying source file " + sourceContainer + ":" + sourcePath + " to " + hostFileName)
+	cmd := exec.Command(dockerCmd, "cp", "-L", sourceContainer+":"+sourcePath, hostFileName)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(output))
+		return fmt.Errorf("encountered error running docker cp to local from %s: %w", sourceContainer, err)
+	}
+	err = os.Chmod(hostFileName, 0655)
+	if err != nil {
+		return fmt.Errorf("encountered error running chmod on %s: %w", hostFileName, err)
+	}
+	return nil
+}
+
 // dockerExec runs the given command on the given container using the given user if not empty
 func (ctx *testContainersTestContext) dockerExec(container string, command []string) (int, string, error) {
 	args := append([]string{"exec", container}, command...)
+	cmd := exec.Command(dockerCmd, args...)
+	output, err := cmd.CombinedOutput()
+	return cmd.ProcessState.ExitCode(), string(output), err
+}
+
+// dockerLogs runs docker log on given container
+func (ctx *testContainersTestContext) dockerLogs(container string) (int, string, error) {
+	args := []string{"logs", container}
 	cmd := exec.Command(dockerCmd, args...)
 	output, err := cmd.CombinedOutput()
 	return cmd.ProcessState.ExitCode(), string(output), err
