574c90e1911a0141fbaed394a700907cac67a725
Chris Morgan <Christopher.Morgan@solacesystems.com>
2023-03-30 14:03:59 -0400

    SOL-90030: Added native ccsmp linux arm library version 100.0.0.810
    Updated includes_linux.go files for multi arch
    
    Updated vendor packaging dummy.go files
    
    Signed-off-by: Chris Morgan <Christopher.Morgan@solacesystems.com>

---
+++ internal/ccsmp/includes_linux_arm64.go
@@ -0,0 +1,42 @@
+// pubsubplus-go-client
+//
+// Copyright 2021-2023 Solace Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ccsmp
+
+/*
+// specific flags for linux static builds in C
+#cgo CFLAGS: -I${SRCDIR}/lib/include
+#cgo LDFLAGS: ${SRCDIR}/lib/linux_arm64/libsolclient.a -lm -ldl -lpthread -lrt
+
+#include <dlfcn.h>
+
+#include <features.h>
+#ifdef __GLIBC__
+#define SOLCLIENT_USING_MUSL 0
+#else
+#define SOLCLIENT_USING_MUSL 1
+#endif
+*/
+import "C"
+
+// Because of some strange behaviour of CCSMP, libdl is not actually required as far as the compiler is concerned.
+// As a result, we must write a function that allows us to link against the dynamic libdl system library. This
+// function should NOT be used as it does nothing and may cause issues.
+func funcToLinkAgainstLibdl() {
+	C.dlerror()
+}
+
+var SolClientRunningOnAlpine bool = C.SOLCLIENT_USING_MUSL == 1
