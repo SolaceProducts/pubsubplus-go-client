ff8ed21adad7d7d632de154d141be20170c61886
TrentDaniel <51713985+TrentDaniel@users.noreply.github.com>
2025-04-28 11:20:42 -0400

    Release 1.10.0 (#29)
    * Updated API version from 1.9.1 to 1.9.2 after release
    
    * Bug EBP-660: Go deprecate SSLv3 and TLSv1
    Removed/deprecated mentions of SSLv3 and and TLSv1.
    
    * Adding useless file to trigger build after brownout.
    
    * EBP-803: Updated CCSMP to 7.33.0.3.
    
    * EBP-511: Removing redundant file
    
    * EBP-803: Fixed merge conflict that wasn't detected locally
    
    * Reapply "Ebp 511 tls13"
    
    This reverts commit a9ff4133503f83e23e9eba456ae2f065ff546eaf.
    
    * EBP-511: Fixing merge issues based on PR feedback.
    
    * EBP-511: Updated generated file in response to feedback.
    
    * EBP-859: Updated Go API version from 1.9.2. to 1.10.0.
    
    ---------
    
    Co-authored-by: Oseme Odigie <103643444+oodigie@users.noreply.github.com>
    Co-authored-by: gszol <38111892+gszol@users.noreply.github.com>
    Co-authored-by: Ragnar <36005865+RagnarPaulson@users.noreply.github.com>

---
+++ internal/ccsmp/ccsmp_session_prop_deprecated.go
@@ -0,0 +1,31 @@
+// pubsubplus-go-client
+//
+// Copyright 2025 Solace Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ccsmp
+
+// This file is hand-maintained with relevant session properties from solClientDeprecated.h
+
+/*
+#cgo CFLAGS: -DSOLCLIENT_PSPLUS_GO
+#include "solclient/solClient.h"
+#include "solclient/solClientDeprecated.h"
+*/
+import "C"
+
+const (
+	// SolClientSessionPropSslExcludedProtocols: This property specifies a comma separated list of excluded SSL protocol(s). Valid SSL protocols are 'SSLv3', 'TLSv1', 'TLSv1.1', 'TLSv1.2'. Default: ::SOLCLIENT_SESSION_PROP_DEFAULT_SSL_EXCLUDED_PROTOCOLS.
+	SolClientSessionPropSslExcludedProtocols = C.SOLCLIENT_SESSION_PROP_SSL_EXCLUDED_PROTOCOLS
+)
